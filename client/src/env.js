bf92a42851a2beb64b332a9f09adc2e946d19290
melanie <mjharv@hotmail.com>
2024-12-10 15:36:25 -0700

    added Schema

--- client/src/env.js
+++ client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-j1kz15wxbfg6xi6m.us.auth0.com'
+export const clientId = 'Eu8EBZA4fPr3Kxj3qk2CKqxml8ToaRc3'
+export const audience = 'https://auth.sadcatloaf.com'
---
+++ server/src/models/TowerEvent.js
@@ -0,0 +1,21 @@
+import { Schema } from "mongoose";
+
+export const TowerEventSchema = new Schema(
+    {
+        id: { type: Schema.ObjectId, required: true, ref: 'Account' },
+        creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' },
+        name: { type: String, minLength: 3, maxLength: 50, required: true },
+        description: { type: String, minLength: 15, maxLength: 1000, required: true },
+        coverImg: { type: String, required: true },
+        location: { type: String, minLength: 1, maxLength: 500 },
+        capacity: { type: Number, minLength: 1, maxLength: 5000, required: true },
+        startDate: { type: Date, required: true },
+        isCanceled: { type: Boolean, required: true },
+        type: { type: String, enum: ['concert, convention,sport,digital'] },
+
+    },
+    {
+        timestamps: true,
+        toJSON: { virtuals: true }
+    }
+)
