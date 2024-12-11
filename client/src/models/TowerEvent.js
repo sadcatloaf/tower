5bc1caa667dbb1ae3f9b46128be57c78e1ed38da
melanie <mjharv@hotmail.com>
2024-12-11 14:00:05 -0700

    events in appstate

---
+++ client/src/models/TowerEvent.js
@@ -0,0 +1,18 @@
+export class Event {
+
+    constructor(data) {
+        this.id = data.id
+        this.creatorId = data.creatorId
+        this.name = data.name
+        this.description = data.description
+        this.capacity = data.capacity
+        this.coverImg = data.coverImg
+        this.createdAt = data.createdAt
+        this.isCanceled = data.isCanceled
+        this.location = data.location
+        this.startDate = data.startDate
+        this.type = data.type
+        this.updatedAt = data.updatedAt
+    }
+
+}
--- client/src/services/TowerEventService.js
+++ client/src/services/TowerEventService.js
@@ -1,11 +1,14 @@
 import { logger } from "@/utils/Logger.js";
 import { api } from "./AxiosService.js";
+import { AppState } from "@/AppState.js";
 
 
 class TowerEventService {
     async getEvents() {
         const response = await api.get('api/events')
         logger.log('Got Events', response.data)
+        const events = response.data.map(eventPOJO => new Event(eventPOJO))
+        AppState.events = events
     }
 
 }
