38d70c29009271d88d1ada093cd960bbe88632fe
melanie <mjharv@hotmail.com>
2024-12-12 13:35:04 -0700

    added ticket to db

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -2,11 +2,13 @@ import mongoose, { model } from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
 import { TowerEventSchema } from '../models/TowerEvent';
+import { TicketSchema } from '../models/Ticket';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
   TowerEvent = mongoose.model('TowerEvent', TowerEventSchema)
+  Ticket = mongoose.model('Ticket', TicketSchema)
 }
 
 export const dbContext = new DbContext()
--- server/src/models/Ticket.js
+++ server/src/models/Ticket.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
-export const ValueSchema = new Schema(
+export const TicketSchema = new Schema(
     {
         accountId: { type: Schema.Types.ObjectId, ref: 'Account', required: true },
         eventId: { type: Schema.ObjectId, required: true, ref: 'TowerEvent' }
@@ -10,14 +10,14 @@ export const ValueSchema = new Schema(
     { timestamps: true, toJSON: { virtuals: true } }
 )
 
-ValueSchema.virtual('profile', {
+TicketSchema.virtual('profile', {
     localField: 'accountId',
     ref: 'Account',
     foreignField: '_id',
     justOne: true,
 })
 
-ValueSchema.virtual('event', {
+TicketSchema.virtual('event', {
     localField: 'eventId',
     ref: 'TowerEvent',
     foreignField: '_id',
